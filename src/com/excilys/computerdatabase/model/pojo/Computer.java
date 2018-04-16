624f907a59b18d52f73c8bc7424cf7703e1b002a
jmdebicki <jmdebicki@excilys.com>
2018-04-16 15:16:11 +0200

    Company and Computer pojos

---
+++ src/com/excilys/computerdatabase/model/pojo/Computer.java
@@ -0,0 +1,57 @@
+package com.excilys.computerdatabase.model.pojo;
+
+import java.time.LocalDate;
+
+public class Computer {
+
+	private int id;
+	private String name;
+	private LocalDate introduced;
+	private LocalDate discontinued;
+	private Company company;
+	
+	public Computer(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(LocalDate introduced) {
+		this.introduced = introduced;
+	}
+
+	public LocalDate getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(LocalDate discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
+}
