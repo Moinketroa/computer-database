28db9b0ecac69bb78981e093a1dde086d2d6fa16
jmdebicki <jmdebicki@excilys.com>
2018-04-24 11:47:55 +0200

    [FEAT-MAVEN] Clean sources in wrong directory

--- src/com/excilys/computerdatabase/exceptions/CompanyNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.computerdatabase.exceptions;
-
-public class CompanyNotFoundException extends CDBException {
-
-	public CompanyNotFoundException(int companyId) {
-		super("Company #" + companyId + " not found. Unable to add the company into the computer.");
-	}
-	
-}
--- src/com/excilys/computerdatabase/exceptions/WrongPageParameterException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.computerdatabase.exceptions;
-
-public class WrongPageParameterException extends CDBException {
-
-	public WrongPageParameterException() {
-		super("Cannot have a negative page offset or less than 1 entity per page");
-	}
-
-}
